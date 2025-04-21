c287dc624e6736db5f1d674481092e9aba5c5b9c
Devin <devin0901708@gmail.com>
2025-04-20 17:13:00 -0700

    Update problem3.py

--- Mod2/problem3.py
+++ Mod2/problem3.py
@@ -1,1 +1,13 @@
+'''
+3. Create a program that prompts for the side lengths of a
+triangle and computes the area using Heron's formula. 
+(https://en.wikipedia.org/wiki/Heron%27s_formula) 
+'''
+a = float(input("what is the length of side 1?" ))
+b = float(input("what is the length of side 2?" ))
+c = float(input("what is the length of side 3?" ))
+s = (0.5*(a+b+c))
 
+semi = s*((s-a)*(s-b)*(s-c))
+
+print (semi**(0.5))
