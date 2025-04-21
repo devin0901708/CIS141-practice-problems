6d6e57283641700ca8530781fa02b20c067adee8
Devin <devin0901708@gmail.com>
2025-04-20 17:22:19 -0700

    Update problem4.py

--- Mod2/problem4.py
+++ Mod2/problem4.py
@@ -1,1 +1,11 @@
+'''
+4. Create a program that prompts the user for their
+birth year and displays a message that says "You are ___ old".
+Use an f-string in your solution to this problem.
+'''
 
+day = int(input("what year were you born? "))
+
+age = (2025-day)
+
+print(f"You are {age} years old.")
