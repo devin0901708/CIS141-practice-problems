0bd22181704a6e4d42f9c624f709ab2e1b2f54c0
Devin <devin0901708@gmail.com>
2025-04-27 20:15:01 -0700

    Create problem5.py

---
+++ Mod3/problem5.py
@@ -0,0 +1,5 @@
+'''
+5. Print 3 words with a | character (called a pipe) in between them. 
+Use the appropriate keyword argument in print() to do so.
+'''
+print ("word1", "word2", "word3", sep=" | ")
